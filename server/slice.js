76dc7f404e90db546d9e8f1261782b53502553f6
rnaythe4th <vladislav.zh.mail@gmail.com>
2025-01-20 16:16:59 +0300

    Updated arguments for CuraEngine call

--- server/slice.js
+++ server/slice.js
@@ -11,7 +11,7 @@ const sliceModel = (
   console.log("hello");
   const outputPath = `${appDir}/outputs/${input_file.split(".")[0]}.gcode`;
   const output = execSync(
-    `CuraEngine slice -v -j ${printer_def} -o ${outputPath} -s roofing_layer_count=3 -l ${filePath}/${input_file}`,
+    `CuraEngine slice -v -j ${printer_def} -o ${outputPath} -s roofing_layer_count="3" -s retraction_combing_avoid_distance=0.8 -s support_z_seam_away_from_model=true -l ${filePath}/${input_file}`,
     { encoding: "utf-8" }
   ); // the default is 'buffer'
 
